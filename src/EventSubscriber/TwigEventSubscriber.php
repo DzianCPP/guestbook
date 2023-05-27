7de9081da13af8b38def32816e3b1452a2f7c11f
DzianCPP <dzian.cpp@gmail.com>
2023-05-27 19:51:48 +0300

    twig global var conferences

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -5,11 +5,21 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 
+use App\Repository\ConferenceRepository;
+
+use Twig\Environment;
+
 class TwigEventSubscriber implements EventSubscriberInterface
 {
+    public function __construct(
+        protected Environment $twig,
+        protected ConferenceRepository $conferenceRepository
+    ) {
+    }
+
     public function onControllerEvent(ControllerEvent $event): void
     {
-        // ...
+        $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
     }
 
     public static function getSubscribedEvents(): array
