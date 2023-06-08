f6a273edd68ab9f6806ab06bebc1e3c5a720358d
DzianCPP <dzian.cpp@gmail.com>
2023-06-08 21:19:14 +0300

    purge cache

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,11 +4,14 @@ namespace App\Controller;
 
 use App\Traits\DataBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
+use Symfony\Component\HttpKernel\HttpCache\StoreInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 use App\Entity\Comment;
 use App\Message\CommentMessage;
@@ -21,13 +24,16 @@ class AdminController extends AbstractController
 {
     use DataBuilder;
 
+    private ?Request $request = null;
+
     public function __construct(
         private Environment $twig,
         private EntityManagerInterface $entityManager,
         private MessageBusInterface $bus,
+        private RequestStack $requestStack,
         private array $data = []
     ) {
-
+        $this->request = $requestStack->getCurrentRequest();
     }
 
     #[Route(
@@ -36,11 +42,10 @@ class AdminController extends AbstractController
         methods: 'GET|HEAD'
     )]
     public function review_comment(
-        Request $request,
         Comment $comment,
         WorkflowInterface $commentStateMachine
     ): Response {
-        $accepted = !$request->query->get('reject');
+        $accepted = !$this->request->query->get('reject');
         if (!$transition = $this->getTransition($comment, $commentStateMachine, $accepted)) {
             return new Response('Comment already reviewed or in a different state');
         }
@@ -55,6 +60,21 @@ class AdminController extends AbstractController
         $this->addItems(['transition' => $transition, 'comment' => $comment]);
 
         return $this->render('admin/review.html.twig', $this->data);
+    }
+
+    #[Route('/admin/http-cache/{uri<.*>}', methods: ['PURGE'])]
+    public function purgeHttpCache(
+        KernelInterface $kernel,
+        StoreInterface $store,
+        string $uri
+    ): Response {
+        if ($kernel->getEnvironment() === 'prod') {
+            return new Response('KO', 400);
+        }
+
+        $store->purge($this->request->getSchemeAndHttpHost() . '/' . $uri);
+
+        return new Response('Done');
     }
 
     private function getTransition(
