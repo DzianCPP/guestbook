6f46e293bb2402080f4d64c18ff5bdb122ea661c
DzianCPP <dzian.cpp@gmail.com>
2023-06-13 21:20:03 +0300

    sessions check

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,24 +2,44 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use App\Entity\Conference;
 use App\Entity\Comment;
 
+use App\Traits\SessionBuilder;
+use App\Traits\DataBuilder;
+
+use Twig\Environment;
+
 class DashboardController extends AbstractDashboardController
 {
+    use SessionBuilder;
+    use DataBuilder;
+
+    private ?Request $request = null;
+
+    public function __construct(
+        private RequestStack $requestStack,
+        private Environment $twig,
+        private array $data = []
+    ) {
+        $this->request = $requestStack->getCurrentRequest();
+        $this->checkIfSessionIsValid();
+    }
+
     #[Route(path: '/admin', name: 'admin')]
     public function admin(): Response
     {
         $urlGenerator = $this->container->get(AdminUrlGenerator::class);
-        
+
         return $this->redirect($urlGenerator->setController(ConferenceCrudController::class)->generateUrl());
     }
 
@@ -35,5 +55,17 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToRoute('Back to website', 'fas fa-home', 'homepage');
         yield MenuItem::linkToCrud('Conferences', 'fas fa-map-marker-alt', Conference::class);
         yield Menuitem::linkToCrud('Comments', 'fa fa-comments', Comment::class);
+    }
+
+    private function checkIfSessionIsValid(): false|Response
+    {
+        if (!$this->isSessionExpired()) {
+            return false;
+        }
+
+        $this->sessionInvalidate();
+        return new Response($this->twig->render('admin/session_expired.html.twig', [
+            'session_expired_message' => 'Oops... your session has expired. Try to re-login or something...'
+        ]));
     }
 }
--- src/Traits/SessionBuilder.php
+++ src/Traits/SessionBuilder.php
@@ -4,13 +4,48 @@ namespace App\Traits;
 
 trait SessionBuilder
 {
+    private const MAX_IDLE_TIME = 60;
+
+    public function sessionStart(): void
+    {
+        $this->request->getSession()->start();
+    }
+
     public function addSessionItem(string $attribute = '', string $value = '')
     {
         if (!$attribute || !$value) {
             return;
         }
 
-        $session = $this->request->getSession();
-        $session->set($attribute, $value);
+        $this->request->getSession()->set($attribute, $value);
+    }
+
+    public function getSessionCreatedAt(): int
+    {
+        return $this->request->getSession()->getMetadataBag()->getCreatedAt();
+    }
+
+    public function getSessionLastUsed(): int
+    {
+        return $this->request->getSession()->getMetadataBag()->getLastUsed();
+    }
+
+    public function isSessionExpired(): bool
+    {
+        if (time() - $this->getSessionLastUsed() > self::MAX_IDLE_TIME) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getLifetime(): int
+    {
+        return $this->request->getSession()->getMetadataBag()->getLifetime();
+    }
+
+    public function sessionInvalidate(): void
+    {
+        $this->request->getSession()->invalidate();
     }
 }
